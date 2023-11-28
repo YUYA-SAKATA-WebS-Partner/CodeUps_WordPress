8e09ef75e2bb33f5145b08a38ac0650c418e1e96
yu <yuprograming@gmail.com>
2023-11-28 22:12:07 +0900

    page-about-us.php完了

--- WordPressTheme/header.php
+++ WordPressTheme/header.php
@@ -228,6 +228,21 @@ $sitemap = esc_url(home_url('/sitemap'));
         <h1 class="sub-mainview__title">Blog</h1>
       </div>
     </div>
+    <?php elseif (is_page('about-us')) : ?>
+    <div class="sub-mainview">
+      <picture class="sub-mainview__image">
+        <source srcset="<?php echo esc_url(get_theme_file_uri()); ?>/assets/images/common/page-about_mv_pc.webp"
+          media="(min-width: 768px)" type="image/webp">
+        <source srcset="<?php echo esc_url(get_theme_file_uri()); ?>/assets/images/common/page-about_mv_pc.jpg"
+          media="(min-width: 768px)">
+        <source srcset="<?php echo esc_url(get_theme_file_uri()); ?>/assets/images/common/page-about_mv_sp.webp"
+          type="image/webp">
+        <img src="<?php echo esc_url(get_theme_file_uri()); ?>/assets/images/common/page-about_mv_sp.jpg" alt="シーサーと青空">
+      </picture>
+      <div class="sub-mainview__box">
+        <h1 class="sub-mainview__title">About us</h1>
+      </div>
+    </div>
     <?php elseif (!(is_front_page() || is_404())) : ?>
     <div class="sub-mainview">
       <picture class="sub-mainview__image">
--- WordPressTheme/page-about-us.php
+++ WordPressTheme/page-about-us.php
@@ -1,6 +1,4 @@
 <?php get_header(); ?>
-<!-- breadcrumbs -->
-<div class="breadcrumbs inner">TOP > 私たちについて</div>
 
 <!-- contents -->
 <div class="page-about sub-page-about-layout">
@@ -8,24 +6,16 @@
     <div class="page-about__container">
       <div class="page-about__images">
         <picture class="page-about__thin-image u-desktop">
-          <source srcset="<?php echo esc_url(get_theme_file_uri('')); ?>/assets/images/common/about2.webp"
-            media="(min-width: 768px)" type="image/webp">
-          <source srcset="<?php echo esc_url(get_theme_file_uri('')); ?>/assets/images/common/about2.jpg"
-            media="(min-width: 768px)">
-          <source srcset="<?php echo esc_url(get_theme_file_uri('')); ?>/assets/images/common/about2-sp.webp"
-            type="image/webp">
-          <img src="<?php echo esc_url(get_theme_file_uri('')); ?>/assets/images/common/about2-sp.jpg"
-            alt="瓦葺きの屋根に乗っているシーサーと青い空">
+          <source srcset="<?php echo esc_url(get_theme_file_uri('')); ?>/assets/images/common/about2.webp" media="(min-width: 768px)" type="image/webp">
+          <source srcset="<?php echo esc_url(get_theme_file_uri('')); ?>/assets/images/common/about2.jpg" media="(min-width: 768px)">
+          <source srcset="<?php echo esc_url(get_theme_file_uri('')); ?>/assets/images/common/about2-sp.webp" type="image/webp">
+          <img src="<?php echo esc_url(get_theme_file_uri('')); ?>/assets/images/common/about2-sp.jpg" alt="瓦葺きの屋根に乗っているシーサーと青い空">
         </picture>
         <picture class="page-about__wide-image">
-          <source srcset="<?php echo esc_url(get_theme_file_uri('')); ?>/assets/images/common/about.webp"
-            media="(min-width: 768px)" type="image/webp">
-          <source srcset="<?php echo esc_url(get_theme_file_uri('')); ?>/assets/images/common/about.jpg"
-            media="(min-width: 768px)">
-          <source srcset="<?php echo esc_url(get_theme_file_uri('')); ?>/assets/images/common/about-sp.webp"
-            type="image/webp">
-          <img src="<?php echo esc_url(get_theme_file_uri('')); ?>/assets/images/common/about-sp.jpg"
-            alt="海中で２匹の黄色い魚が向かい合っている様子">
+          <source srcset="<?php echo esc_url(get_theme_file_uri('')); ?>/assets/images/common/about.webp" media="(min-width: 768px)" type="image/webp">
+          <source srcset="<?php echo esc_url(get_theme_file_uri('')); ?>/assets/images/common/about.jpg" media="(min-width: 768px)">
+          <source srcset="<?php echo esc_url(get_theme_file_uri('')); ?>/assets/images/common/about-sp.webp" type="image/webp">
+          <img src="<?php echo esc_url(get_theme_file_uri('')); ?>/assets/images/common/about-sp.jpg" alt="海中で２匹の黄色い魚が向かい合っている様子">
         </picture>
       </div>
       <div class="page-about__body">
@@ -48,66 +38,20 @@
       <h2 class="section-title__sub">フォト</h2>
     </div>
     <div class="gallery__body">
-      <picture class="gallery__image js-modal-trigger">
-        <source srcset="<?php echo esc_url(get_theme_file_uri('')); ?>/assets/images/common/gallery1_pc.webp"
-          media="(min-width:768px)" type="image/webp">
-        <source srcset="<?php echo esc_url(get_theme_file_uri('')); ?>/assets/images/common/gallery1_pc.jpg"
-          media="(min-width:768px)">
-        <source srcset="<?php echo esc_url(get_theme_file_uri('')); ?>/assets/images/common/gallery1_sp.webp"
-          type="image/webp">
-        <img src="<?php echo esc_url(get_theme_file_uri('')); ?>/assets/images/common/gallery1_sp.jpg"
-          alt="珊瑚礁に小さな赤い魚が群がっている様子">
-      </picture>
-      <picture class="gallery__image js-modal-trigger">
-        <source srcset="<?php echo esc_url(get_theme_file_uri('')); ?>/assets/images/common/gallery2_pc.webp"
-          media="(min-width:768px)" type="image/webp">
-        <source srcset="<?php echo esc_url(get_theme_file_uri('')); ?>/assets/images/common/gallery2_pc.jpg"
-          media="(min-width:768px)">
-        <source srcset="<?php echo esc_url(get_theme_file_uri('')); ?>/assets/images/common/gallery2_sp.webp"
-          type="image/webp">
-        <img src="<?php echo esc_url(get_theme_file_uri('')); ?>/assets/images/common/gallery2_sp.jpg"
-          alt="浜辺から見える島や船の景色">
-      </picture>
-      <picture class="gallery__image js-modal-trigger">
-        <source srcset="<?php echo esc_url(get_theme_file_uri('')); ?>/assets/images/common/gallery3_pc.webp"
-          media="(min-width:768px)" type="image/webp">
-        <source srcset="<?php echo esc_url(get_theme_file_uri('')); ?>/assets/images/common/gallery3_pc.jpg"
-          media="(min-width:768px)">
-        <source srcset="<?php echo esc_url(get_theme_file_uri('')); ?>/assets/images/common/gallery3_sp.webp"
-          type="image/webp">
-        <img src="<?php echo esc_url(get_theme_file_uri('')); ?>/assets/images/common/gallery3_sp.jpg"
-          alt="２匹の魚が海底を泳いでいる様子">
-      </picture>
-      <picture class="gallery__image js-modal-trigger">
-        <source srcset="<?php echo esc_url(get_theme_file_uri('')); ?>/assets/images/common/gallery4_pc.webp"
-          media="(min-width:768px)" type="image/webp">
-        <source srcset="<?php echo esc_url(get_theme_file_uri('')); ?>/assets/images/common/gallery4_pc.jpg"
-          media="(min-width:768px)">
-        <source srcset="<?php echo esc_url(get_theme_file_uri('')); ?>/assets/images/common/gallery4_sp.webp"
-          type="image/webp">
-        <img src="<?php echo esc_url(get_theme_file_uri('')); ?>/assets/images/common/gallery4_sp.jpg"
-          alt="黄色い魚が泳いでいる様子">
-      </picture>
-      <picture class="gallery__image js-modal-trigger">
-        <source srcset="<?php echo esc_url(get_theme_file_uri('')); ?>/assets/images/common/gallery5_pc.webp"
-          media="(min-width:768px)" type="image/webp">
-        <source srcset="<?php echo esc_url(get_theme_file_uri('')); ?>/assets/images/common/gallery5_pc.jpg"
-          media="(min-width:768px)">
-        <source srcset="<?php echo esc_url(get_theme_file_uri('')); ?>/assets/images/common/gallery5_sp.webp"
-          type="image/webp">
-        <img src="<?php echo esc_url(get_theme_file_uri('')); ?>/assets/images/common/gallery5_sp.jpg"
-          alt="小さな魚の群れが泳いでいる様子">
-      </picture>
-      <picture class="gallery__image js-modal-trigger">
-        <source srcset="<?php echo esc_url(get_theme_file_uri('')); ?>/assets/images/common/gallery6_pc.webp"
-          media="(min-width:768px)" type="image/webp">
-        <source srcset="<?php echo esc_url(get_theme_file_uri('')); ?>/assets/images/common/gallery6_pc.jpg"
-          media="(min-width:768px)">
-        <source srcset="<?php echo esc_url(get_theme_file_uri('')); ?>/assets/images/common/gallery6_sp.webp"
-          type="image/webp">
-        <img src="<?php echo esc_url(get_theme_file_uri('')); ?>/assets/images/common/gallery6_sp.jpg"
-          alt="２匹のオレンジ色の魚が珊瑚礁の近くを泳いでいる様子">
-      </picture>
+      <?php
+      $galleryImages = SCF::get_option_meta('theme_options_gallery', 'about_gallery');
+      foreach ($galleryImages as $galleryImage) {
+        $imageUrl = wp_get_attachment_image_src($galleryImage['gallery'], 'large');
+        $alt = esc_html($galleryImage['alt-text']);
+        if ($imageUrl) :
+      ?>
+          <picture class="gallery__image js-modal-trigger">
+            <img src="<?php echo $imageUrl[0]; ?>" alt="<?php echo $alt; ?>">
+          </picture>
+      <?php
+        endif;
+      };
+      ?>
     </div>
   </div>
 </div>
