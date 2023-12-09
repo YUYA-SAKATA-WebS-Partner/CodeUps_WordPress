7f7d442ed97538dd17426be1ac99926e89557634
yu <yuprograming@gmail.com>
2023-12-10 05:30:45 +0900

    terms-of-service完了

--- WordPressTheme/header.php
+++ WordPressTheme/header.php
@@ -337,6 +337,22 @@ $sitemap = esc_url(home_url('/sitemap'));
         <h1 class="sub-mainview__title">Privacy Policy</h1>
       </div>
     </div>
+    <?php elseif (is_page('terms-of-service')) : ?>
+    <div class="sub-mainview">
+      <picture class="sub-mainview__image">
+        <source srcset="<?php echo esc_url(get_theme_file_uri()); ?>/assets/images/common/page-sitemap_mv_pc.webp"
+          media="(min-width: 768px)" type="image/webp">
+        <source srcset="<?php echo esc_url(get_theme_file_uri()); ?>/assets/images/common/page-sitemap_mv_pc.jpg"
+          media="(min-width: 768px)">
+        <source srcset="<?php echo esc_url(get_theme_file_uri()); ?>/assets/images/common/page-sitemap_mv_sp.webp"
+          type="image/webp">
+        <img src="<?php echo esc_url(get_theme_file_uri()); ?>/assets/images/common/page-sitemap_mv_sp.jpg"
+          alt="広い砂浜と波打ち際の風景">
+      </picture>
+      <div class="sub-mainview__box">
+        <h1 class="sub-mainview__title">Terms of Service</h1>
+      </div>
+    </div>
     <?php elseif (!(is_front_page() || is_404())) : ?>
     <div class="sub-mainview">
       <picture class="sub-mainview__image">
